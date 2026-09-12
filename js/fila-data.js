8215225e7def54835cb1baefb1510675c2671302
harkonimstudio <trovemelhorjogo@gmail.com>
2026-09-12 02:27:35 -0300

    Update fila-data.js

--- js/fila-data.js
+++ js/fila-data.js
@@ -33,8 +33,38 @@ const FILA_PRODUCAO = [
   {
     "nome": "Yani neko diorama",
     "codigo": "0009",
+    "etapa": "impressao"
+  },
+  {
+    "nome": "Rias gremory",
+    "codigo": "0010",
+    "etapa": "aguardando"
+  },
+  {
+    "nome": "yani neko diorama",
+    "codigo": "0011",
+    "etapa": "aguardando"
+  },
+  {
+    "nome": "Reze",
+    "codigo": "0012",
+    "etapa": "aguardando"
+  },
+  {
+    "nome": "Yani neko diorama",
+    "codigo": "0013",
+    "etapa": "aguardando"
+  },
+  {
+    "nome": "Ghislaine",
+    "codigo": "HKM-W011",
+    "etapa": "aguardando"
+  },
+  {
+    "nome": "Yani neko sitted",
+    "codigo": "HKM-F012",
     "etapa": "aguardando"
   }
 ];
 
-const ULTIMA_ATUALIZACAO_FILA = "04/09/2026";
+const ULTIMA_ATUALIZACAO_FILA = "12/09/2026";
